99b7a3a4a41a3d620fd204a1f0551079f5d5bfc8
S.R JHA <shrutiranjanjha274@gmail.com>
2024-07-23 16:40:19 +0530

    Update middleware to store token as a cookie

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,18 +1,33 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 
-function authenticate(req, res, next) {
-  const token = req.header("Authorization")?.split(" ")[1];
-  if (!token) {
-    return res.status(401).json({ message: "No token, authorization denied" });
-  }
-
+const auth = async (req, res, next) => {
   try {
+    const token = req.cookies.token;
+    console.log("Token received:", token);
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "No token, authorization denied" });
+    }
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("Decoded token:", decoded);
+
     req.user = decoded.user;
+
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    console.log("User found:", user);
     next();
-  } catch (error) {
+  } catch (err) {
+    console.log("Error in auth middleware:", err);
     res.status(401).json({ message: "Token is not valid" });
   }
-}
+};
 
-module.exports = authenticate;
+module.exports = auth;
