4f2ca8b22af889c5d31d0eea29213307b6e4824e
S.R JHA <shrutiranjanjha274@gmail.com>
2024-08-02 17:54:37 +0530

    Added schema for managing product reviews and related information.

---
+++ models/reviewModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+reviewSchema.pre("save", function (next) {
+  this.updated_at = Date.now();
+  next();
+});
+
+module.exports = mongoose.model("Review", reviewSchema);
