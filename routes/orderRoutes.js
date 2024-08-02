e50baf9f2e412426aaf8319e4a6039b7f3841aea
S.R JHA <shrutiranjanjha274@gmail.com>
2024-08-02 17:58:24 +0530

    Created routes for order management and status updates.

---
+++ routes/orderRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const orderController = require("../controller/orderController");
+const authMiddleware = require("../middlewares/authMiddleware");
+const checkAdmin = require("../middlewares/isAdmin")
+
+// Create a new order
+router.post("/order", authMiddleware, orderController.createOrder);
+
+// Get a list of orders
+router.get("/orders", authMiddleware, orderController.getOrders);
+
+// Get order by ID
+router.get("/order/:id", authMiddleware, orderController.getOrderById);
+
+// Update order status
+router.put(
+  "/order/:id",
+  authMiddleware,
+  checkAdmin,
+  orderController.updateOrderStatus
+);
+
+// Delete an order
+router.delete("/order/:id", authMiddleware, orderController.deleteOrder);
+
+module.exports = router;
