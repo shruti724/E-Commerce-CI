9f9424d1a5ecba46a8de6460c0b98847771030ac
S.R JHA <shrutiranjanjha274@gmail.com>
2024-08-02 17:29:59 +0530

    Enhanced uploadMiddleware to handle file uploads correctly.

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -11,23 +11,20 @@ const ensureDirExists = (dir) => {
 
 // Set up multer for file uploads
 const storage = multer.diskStorage({
-  destination: (req, file, cb)=> {
+  destination: (req, file, cb) => {
     let uploadDir;
-//  console.log(req.params);
- brandId = req.params.id
-    // Determine the appropriate directory based on the route parameter
-    if (brandId) {
-       
-      uploadDir = path.join(
-        __dirname,
-        "../public/images/brands",
-        brandId
-      );
-    } else if (req.params.categoryId) {
+
+    // Check for route parameters and set the upload directory accordingly
+    const { id, productId, categoryId } = req.params;
+    if (id) {
+      uploadDir = path.join(__dirname, "../public/images/brands", id);
+    } else if (productId) {
+      uploadDir = path.join(__dirname, "../public/images/products", productId);
+    } else if (categoryId) {
       uploadDir = path.join(
         __dirname,
         "../public/images/categories",
-        req.params.categoryId
+        categoryId
       );
     } else {
       return cb(new Error("Invalid upload route"), false);
@@ -38,21 +35,21 @@ const storage = multer.diskStorage({
 
     cb(null, uploadDir);
   },
-  filename: function (req, file, cb) {
+  filename: (req, file, cb) => {
     cb(null, file.originalname);
   },
 });
 
 const upload = multer({
   storage: storage,
-  fileFilter: function (req, file, cb) {
+  fileFilter: (req, file, cb) => {
     const ext = path.extname(file.originalname);
     if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
       return cb(new Error("Only images are allowed"), false);
     }
     cb(null, true);
   },
-  limits: { fileSize: 1024 * 1024 * 5 }, // 5MB size limit
+  limits: { fileSize: 1024 * 1024 * 5 }, 
 });
 
 module.exports = upload;
