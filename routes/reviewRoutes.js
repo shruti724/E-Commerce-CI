ca3fc7543def753f5f6a9f962ba6e1660495fcea
S.R JHA <shrutiranjanjha274@gmail.com>
2024-08-02 17:59:16 +0530

    Added routes for handling product review API endpoints.

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const reviewController = require("../controller/reviewController");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+// Create a new review
+router.post("/reviews", authMiddleware, reviewController.createReview);
+
+// Get a list of reviews
+router.get("/reviews", reviewController.getReviews);
+
+// Get review by ID
+router.get("/reviews/:id", reviewController.getReviewById);
+
+// Update review
+router.put("/reviews/:id", authMiddleware, reviewController.updateReview);
+
+// Delete a review
+router.delete(
+  "/reviews/:id",
+  authMiddleware,
+  reviewController.deleteReview
+);
+
+module.exports = router;
