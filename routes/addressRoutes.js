e12708dc2e755c262a5ceb96e14e1bcaa7f1af71
S.R JHA <shrutiranjanjha274@gmail.com>
2024-08-06 13:27:28 +0530

    Added getUserWithAddresses and deleteAddress functions; updated createAddress and addANewAddress for enhanced functionality

--- routes/addressRoutes.js
+++ routes/addressRoutes.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 const {
   createAddress,
+  addANewAddress,
   getUserWithAddresses,
+  deleteAddress,
 } = require("../controller/addressController");
 
 const authMiddleware = require("../middlewares/authMiddleware");
 
-router.post("/addresses", authMiddleware, createAddress);
+router.post("/address", authMiddleware, createAddress);
 router.get("/addresses", authMiddleware, getUserWithAddresses);
-
+router.post("/address/other", authMiddleware, addANewAddress);
+router.delete("/address/:id", authMiddleware, deleteAddress);
 module.exports = router;
