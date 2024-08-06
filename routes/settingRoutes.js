e3a1e15bbb3cc9eb196440d769c67008cc13cf14
S.R JHA <shrutiranjanjha274@gmail.com>
2024-08-06 15:28:40 +0530

    Added routes for setting management

---
+++ routes/settingRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllSettings,
+  getSettingByKey,
+  upsertSetting,
+  deleteSetting,
+} = require("../controller/settingController");
+const authMiddleware = require("../middlewares/authMiddleware");
+const checkAdmin = require("../middlewares/isAdmin")
+
+// Middleware for authentication and admin check
+
+
+
+router.get("/settings", authMiddleware, checkAdmin, getAllSettings);
+router.get("/settings/:key", authMiddleware, checkAdmin, getSettingByKey);
+router.put("/settings/:key", authMiddleware, checkAdmin, upsertSetting);
+router.delete("/settings/:key", authMiddleware, checkAdmin, deleteSetting);
+
+module.exports = router;
